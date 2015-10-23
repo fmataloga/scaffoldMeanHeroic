c26271154f35ddbc845a79e2cae468793513d129
Felix Mata <felixrmatac@gmail.com>
2015-10-22 23:57:01 -0500

    Cambios en CRUD

--- public/javascripts/setup/setupController.js
+++ public/javascripts/setup/setupController.js
@@ -1,16 +1,17 @@
-.
-controller('setupController',
+.controller('setupController',
     ['$scope',
         function ($scope) {
-
+            $scope.fieldName = [];
+            $scope.showOnView = [];
+            var stringFields = "";
+            var stringDataTypes = "";
+            var stringShowOnView = "";
 
 //initialize data on first Row
             $scope.collection = [{
-
                 field: '',
                 dataType: '',
                 showOnView: ''
-
             }];
 
             // remove the "Remove" Header and Body when only left one document
@@ -21,29 +22,24 @@ controller('setupController',
                     return true;
             };
 
-            $scope.fieldName = [];
-            $scope.showOnView = [];
-            var stringFields = "";
-            var stringDataTypes = "";
-            var stringShowOnView = "";
 
             $scope.dataTypes = [
-                    {
-                        id: 1,
-                        name: 'String'
-                    },
-                    {
-                        id: 2,
-                        name: 'Number'
-                    },
-                    {
-                        id: 3,
-                        name: 'Boolean'
-                    },
-                    {
-                        id: 4,
-                        name: 'Array'
-                    }];
+                {
+                    id: 1,
+                    name: 'String'
+                },
+                {
+                    id: 2,
+                    name: 'Number'
+                },
+                {
+                    id: 3,
+                    name: 'Boolean'
+                },
+                {
+                    id: 4,
+                    name: 'Array'
+                }];
 
             // expose a function to add new (blank) rows to the model/table
             $scope.addRow = function () {
@@ -53,19 +49,13 @@ controller('setupController',
                     dataType: $scope.dataTypes[0],
                     showOnView: $scope.showOnView[0]
                 });
-
-
-                $scope.prueba = $scope.collection;
-
             }
 
             $scope.removeRow = function (index) {
                 $scope.collection.splice(index, 1);
             }
 
-
             $scope.validate = function () {
-
                 var cont = 0;
                 angular.forEach($scope.collection, function (value, key) {
                     cont++;
@@ -78,14 +68,12 @@ controller('setupController',
                         stringDataTypes += value.dataType.name;
                         stringShowOnView += value.showOnView;
                     }
-
                 });
                 console.log(
-                    "fields = " + stringFields + " " +
-                    "dataTypes = " + stringDataTypes + " " +
-                    "showOnView = " + stringShowOnView
+                    "schemeName = "  + '"' + $scope.schemeName + '"' + "\n" +
+                    "fields = " + '"' +stringFields + '"' + "\n" +
+                    "dataTypes = " + '"' +stringDataTypes + '"' + "\n" +
+                    "showOnView = " + '"' +stringShowOnView  + '"'
                 )
             }
-
-
         }])
