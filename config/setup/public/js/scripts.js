d226e39110c9d6fe3d74d8d0bd1187b708842dfb
fmataloga <fmata@logasystems.com>
2015-11-04 17:37:05 -0500

    Dynamic Layout in setup and favicon.ico

--- config/setup/public/js/scripts.js
+++ config/setup/public/js/scripts.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
     $('#fullpage').fullpage({
-        anchors: ['firstPage', 'secondPage', '3rdPage', '4rdPage'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
         sectionsColor: ['#FFFFFF', '#E6E6E6', '#CFCFCF', '#BABABA', '#A7A7A7'],
+        //scrollOverflow: true,
+        autoScrolling: false,
         navigation: true,
-        navigationPosition: 'right',
-        responsiveWidth: 900
+        navigationPosition: 'right'
     });
 });
