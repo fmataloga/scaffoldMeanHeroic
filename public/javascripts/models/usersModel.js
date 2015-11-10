8897f98de8475dd9a1f5cf0e93011668a7d6e167
afvs1989 <afvs@outlook.es>
2015-11-10 17:38:20 -0500

    Add method constructorModel

--- public/javascripts/models/usersModel.js
+++ public/javascripts/models/usersModel.js
@@ -1,5 +1,6 @@
 .service('usersModel', function ($optimumModel) {
 	var model = new $optimumModel();
 	model.url = '/api/users';
+	model.constructorModel = ['username','password','rol'];
 	return model;
 })
